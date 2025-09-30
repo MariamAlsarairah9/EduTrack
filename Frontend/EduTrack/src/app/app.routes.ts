e062936127c60a0bc141005abbc96db1ad18eaf0
tasneem <tasneemghazal435@gmail.com>
2025-09-30 15:00:20 +0300

    add assignment comp

--- Frontend/EduTrack/src/app/app.routes.ts
+++ Frontend/EduTrack/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Teacher } from './components/teacher/teacher';
+import { TeacherAssignment } from './components/teacher-assignment/teacher-assignment';
 export const routes: Routes = [
 
 {path : "" ,redirectTo: '/teacher' ,pathMatch:'full'},
  { path: 'teacher', component: Teacher },
+ { path: 'teacherassignment', component: TeacherAssignment },
 
 ];
