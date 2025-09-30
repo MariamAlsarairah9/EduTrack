108ad6438237cc8d674fcf1b88dc2276d2dd1bb9
Dania Da'seen <daniadaseen@gmail.com>
2025-09-30 20:10:42 +0300

    Grades and parent controllers , dtos , Models

---
+++ EduTrack/DTOs/Parent/ParentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EduTrack.DTOs.Parent
+{
+    public class ParentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public long StudentId { get; set; }
+
+
+    }
+}
---
+++ EduTrack/Model/Grades.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduTrack.Model
+{
+    public class Grades
+    {
+        [Key]
+        public long Id { get; set; }
+        public long StudentId { get; set; }
+
+        [MaxLength(50)]
+        public string SubjectName { get; set; }
+        public long score { get; set; }
+
+
+    }
+}
