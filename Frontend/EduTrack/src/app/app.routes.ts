ec5fcdcfcb2390184cc4e696e5e9288e8db82242
tasneem <tasneemghazal435@gmail.com>
2025-10-12 19:06:11 +0300

    assignment(add)  and create login component (html,css)

--- Frontend/EduTrack/src/app/app.routes.ts
+++ Frontend/EduTrack/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Teacher } from './components/teacher/teacher';
 import { TeacherAttendance } from './components/teacher-attendance/teacher-attendance';
 import { TeacherAssignment } from './components/teacher-assignment/teacher-assignment';
 import { TeacherGrade } from './components/teacher-grade/teacher-grade';
+import { Login } from './components/login/login';
 export const routes: Routes = [
 
 {path : "" ,redirectTo: '/teacher' ,pathMatch:'full'},
@@ -12,6 +13,7 @@ export const routes: Routes = [
  
  { path: 'teacherassignment', component: TeacherAssignment },
   { path: 'teacherGrade', component: TeacherGrade },
+ { path: 'login', component: Login },
 
 
 ];
